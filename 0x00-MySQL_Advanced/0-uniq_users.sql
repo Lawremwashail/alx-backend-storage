dcfa9403e9b37e9e5c0573c36b51ba25db5a6082
Lawremwashail <lawremwash@gmail.com>
2024-10-17 11:55:35 +0300

    uniqu users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- Creates table users, if it exists it will not fail
+-- has attributes id, email, name
+-- id is integer, never null, out-increases and a primary key
+-- email is a string with 255 characters, never null and unique
+-- name is a string with 255 characters
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)	
+);

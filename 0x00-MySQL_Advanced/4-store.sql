1f2b3f572d61d5fb72d79046e01b336a1bc354a3
Lawremwashail <lawremwash@gmail.com>
2024-10-17 13:11:17 +0300

    4stores

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -8,9 +8,10 @@ DELIMETER $$
 CREATE TRIGGER update_quantity_after_order
 AFTER INSERT ON orders
 FOR EACH ROW
-	BEGIN
-		UPDATE items
-		SET quantity = quantity - NEW.quantity
-		WHERE item_id = NEW.item_id;
-	END$$
+BEGIN
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
+END$$
+
 DELIMETER;

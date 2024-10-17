6169ac5cd5d17db491d8439c3f742607071474df
Lawremwashail <lawremwash@gmail.com>
2024-10-17 13:23:19 +0300

    5valid email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Creates a trigger that resets 'valid_email' only when the email has been changed.
+-- Change the delimiter to allow for trigger creation
+-- If the email has changed, reset 'valid_email' to false (or 0)
+
+DELIMETER $$
+CREATE TRIGGER reset_email_on_change
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END$$
+DELIMETER ;

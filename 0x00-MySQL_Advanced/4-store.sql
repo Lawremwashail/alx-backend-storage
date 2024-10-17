264729ab973bf19dd0f60651c4d9a7750299c4b9
Lawremwashail <lawremwash@gmail.com>
2024-10-17 13:14:22 +0300

    stores

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,17 +1,11 @@
--- Create a trigger that decreases the quantity of an item after adding a new order-- Change the delimiter to allow for trigger creation
+-- Create a trigger that decreases the quantity of an item after adding a new order
 -- Update the quantity in the 'items' table after a new order is added.
 -- Decrease the item's quantity by the ordered quantity
 -- Ensure we update the correct item based on item_id in the new order
 
-DELIMETER $$
-
 CREATE TRIGGER update_quantity_after_order
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-	UPDATE items
-	SET quantity = quantity - NEW.number
-	WHERE name = NEW.item_name;
-END$$
-
-DELIMETER;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;

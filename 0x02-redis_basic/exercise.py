caea39adc49fba357665b60fbac938460e9da37c
Lawremwashail <lawremwash@gmail.com>
2024-10-25 19:18:47 +0300

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,23 +3,21 @@
 Writing strings to Redis
 """
 
-
 import redis
 import uuid
 from typing import Union
 
 redis_client = redis.Redis()
 
+
 def initialize_cache() -> None:
     """
     Flushes Redis database
     """
-    
     redis_client.flushdb()
-    
-def store_data(data: Union[str, bytes, int, float]) -> str:
+
+    def store_data(data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
-        
         redis_client.set(key, data)
-        
+
         return key
